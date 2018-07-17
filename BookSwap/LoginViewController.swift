3de34e990ed21120474a095c599795cdc4eeee65
David Shapiro <David@davids-mbp-3.attlocal.net>
2018-07-16 23:06:26 -0700

    Firebase login/registration complete

---
+++ BookSwap/LoginViewController.swift
@@ -0,0 +1,44 @@
+//
+//  LoginViewController.swift
+//  BookSwap
+//
+//  Created by David Shapiro on 7/16/18.
+//  Copyright © 2018 David Shapiro. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class LoginViewController: UIViewController {
+
+    //username and password outlets
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    //user login
+    @IBAction func loginPressed(_ sender: Any) {
+        
+        Auth.auth().signIn(withEmail: emailTextField.text!, password: passwordTextField.text!) { (user, error) in
+            
+            if error != nil{
+                print(error!)
+            }else{
+                print("Login Successful")
+                
+                self.performSegue(withIdentifier: "loginSuccess", sender: self)
+            }
+        }
+    }
+}
---
+++ BookSwap/RegisterViewController.swift
@@ -0,0 +1,41 @@
+//
+//  RegisterViewController.swift
+//  BookSwap
+//
+//  Created by David Shapiro on 7/16/18.
+//  Copyright © 2018 David Shapiro. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class RegisterViewController: UIViewController {
+
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func registerPressed(_ sender: Any) {
+        
+        Auth.auth().createUser(withEmail: emailTextField.text!, password: passwordTextField.text!) { (user, error) in
+            
+            if error != nil{
+                print(error!)
+            }else{
+                print("Registration Sucessful")
+                
+                self.performSegue(withIdentifier: "registrationSuccess", sender: self)
+            }
+        }
+    }
+}
