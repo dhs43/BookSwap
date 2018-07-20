79cdcb2c5feae6982d5695aa17e53a69681a34ef
dhs43 <davidshap3@gmail.com>
2018-07-20 02:37:47 -0700

    File reorganization and added app icon

---
+++ BookSwap/GoogleBooksSearch.swift
@@ -0,0 +1,30 @@
+//
+//  GoogleBooksSearch.swift
+//  BookSwap
+//
+//  Created by David Shapiro on 7/20/18.
+//  Copyright © 2018 David Shapiro. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+
+class GoogleBooksSearch: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func searchGoogleBooks(){
+        
+        
+        
+    }
+}
